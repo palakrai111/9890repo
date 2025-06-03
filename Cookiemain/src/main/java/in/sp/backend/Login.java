c31e1db8bda41d8a763123715780d40ed9dcd699
palakrai1987 <its.palak11@gmail.com>
2025-06-03 12:43:02 +0530

    "cookies"

---
+++ Cookiemain/src/main/java/in/sp/backend/Login.java
@@ -0,0 +1,61 @@
+package in.sp.backend;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Login
+ */
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw  = response.getWriter();
+		response.setContentType("text/html");
+		String uname = request.getParameter("uname");
+		if(uname.equals("abc"))
+		{
+			Cookie ck = new Cookie("un",uname);
+			response.addCookie(ck);
+			RequestDispatcher rd= request.getRequestDispatcher("Profile");
+			rd.forward(request, response);
+		}else
+		{
+			pw.print("invalid");
+			RequestDispatcher rd= request.getRequestDispatcher("login.html");
+			rd.include(request, response);
+		}
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ Cookiemain/src/main/java/in/sp/backend/Profile.java
@@ -0,0 +1,57 @@
+package in.sp.backend;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Profile
+ */
+public class Profile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Profile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("link.html").include(request, response);  
+		PrintWriter pw  = response.getWriter();
+		response.setContentType("text/html");
+		
+		Cookie ck[] = request.getCookies();
+		if(ck!=null)
+		{
+			String str = ck[0].getValue();
+			pw.print("welcome user"+ " " + str);
+		}else
+		{
+			pw.print("cookies not present");
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
